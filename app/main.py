e6a2a5d9eb2a7e2e3bbe785addbe777bac293b6e
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-12-09 19:41:44 +0530

    paths

--- app/main.py
+++ app/main.py
@@ -1,27 +1,36 @@
 import sys
+import shutil
 
-BUILTINS = {
-    "exit": lambda code=0, *_: sys.exit(int(code)),
-    "echo": lambda *args: print(" ".join(args)),
-    "type": lambda cmd=None, *_: print(
-        f"{cmd} is a shell builtin" if cmd in BUILTINS else f"{cmd}: not found"
-    ),
-}
+builtins: list = ["echo", "exit", "type"]
+
 
 def main():
-    
     while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
+        prompt: str = "$ "
+        sys.stdout.write(prompt)
+        command: str = input().strip()
+        command: dict = {
+            "base": command.split(" ")[0],
+            "arguments": command.split(" ")[1:],
+        }
 
-        usr_input = input().split()
-        cmd = usr_input[0]
-        args = usr_input[1:]
-        if cmd in BUILTINS:
-            BUILTINS[cmd](*args)
-        else:
-            print(f"{cmd}: command not found")
+        match command["base"]:
+            case "exit":
+                exit()
+            case "echo":
+                print(" ".join(command["arguments"]))
+            case "type":
+                if command["arguments"][0] in builtins:
+                    print(f"{command['arguments'][0]} is a shell builtin")
+                elif path := shutil.which(command["arguments"][0]):
+                    print(f"{command['arguments'][0]} is {path}")
+                else:
+                    print(f"{command['arguments'][0]}: not found")
 
+            case "":
+                pass
+            case _:
+                print(f"{command['base']}: command not found")
 
 
 if __name__ == "__main__":
