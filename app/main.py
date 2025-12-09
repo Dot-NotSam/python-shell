6247889dd2fd48ddfc9ad90ea5131923e6e8c3da
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-12-09 19:35:15 +0530

    added exit

--- app/main.py
+++ app/main.py
@@ -4,15 +4,13 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-
-    command = input()
-    print(f"{command}: command not found")
     
-    if(command == "exit"):
-        sys.exit()
-    main()
-
-    exit
+    while True:
+        command = input()
+        if command == "exit":
+            break
+        print(f"{command}: command not found")
+        sys.stdout.write("$ ")
 
 
 
