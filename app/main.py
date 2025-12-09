c72835d4117d065b6e3df31b27703c2b4e976af3
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-12-09 20:08:55 +0530

    >>

--- app/main.py
+++ app/main.py
@@ -1,73 +1,76 @@
-import sys, os, shutil, shlex, subprocess
+from datetime import datetime
+import os
+import subprocess
+import sys
+import shutil
+import shlex
+from xmlrpc.client import DateTime
 
-# list can contain elements of multiple data types
-builtins: list = ["exit", "echo", "type", "pwd"]
-PATH = os.getenv("PATH", "")
-paths = PATH.split(":")
-HOME = os.getenv("HOME", "")
+
+def command_type(command):
+    if command in commands:
+        print(f"{command} is a shell builtin")
+    elif path := shutil.which(command):
+        print(f"{command} is {path}")
+    else:
+        print(f"{command}: not found")
 
 
-def find_exec(cmd: str):
-    for path in paths:
-        full_path = f"{path}/{cmd}"
+def change_path(path):
+    if path != "~":
         try:
-            with open(full_path):
-                # os.X_OK: Checks if the path is executable.
-                if os.access(full_path, os.X_OK):
-                    return full_path
+            os.chdir(path)
         except FileNotFoundError:
-            continue
-    return None
+            print(f"cd: {path}: No such file or directory")
+    else:
+        os.chdir(os.environ["HOME"])
+
+
+def getCurrentPath():
+    if os.getcwd() == os.environ["HOME"]:
+        return "~"
+    else:
+        return os.getcwd()
+
+
+commands = {
+    "exit": lambda: os._exit(0),
+    "echo": lambda *args: print(" ".join(args)),
+    "type": lambda command: command_type(command),
+    "pwd": lambda: print(os.getcwd()),
+    "cd": lambda path: change_path(path),
+}
 
 
 def main():
     while True:
-        sys.stdout.write("$ ")
-        command_inp: str = input().strip()
-        # removes all leading and trailing whitespace characters, including spaces, tabs (\t), newlines (\n), and carriage returns (\r)
-        # reassigning the command variable
-        command: dict = {
-            "base": shlex.split(command_inp)[0],
-            "args": shlex.split(command_inp)[1:],
-        }
-        # print(shlex.split(command_inp))
-        if ">" in command_inp or "1>" in command_inp:
-            os.system(command_inp)
+        # sys.stdout.write(f"{datetime.now().date()} | { getCurrentPath() } $ ")
+        sys.stdout.write(f"$ ")
+
+        command_with_args = input().strip()
+        command_with_args = shlex.split(command_with_args)
+
+        command = command_with_args[0]
+
+        if (
+            ">" in command_with_args
+            or "1>" in command_with_args
+            or "2>" in command_with_args
+            or ">>" in command_with_args
+            or "1>>" in command_with_args
+            or "2>>" in command_with_args
+        ):
+            os.system(" ".join(command_with_args))
             continue
-        match command["base"]:
-            case "exit":
-                exit()
-            case "echo":
-                # separator.join(iterable_of_strings)
-                print(" ".join(command["args"]))
-            case "type":
-                if command["args"][0] in builtins:
-                    print(f"{command["args"][0]} is a shell builtin")
-                # shutil.which() return the path to an executable which would be run if the given cmd was called. If no cmd would be called, return None.
-                elif path := shutil.which(command["args"][0]):
-                    print(f"{command["args"][0]} is {path}")
-                # path=shutil.which(command["args"][0])
-                # if path:
-                #     print(f"{command["args"][0]} is path")
-                else:
-                    print(f"{command["args"][0]}: not found")
-            case "pwd":
-                print(os.getcwd())
-            case "cd":
-                if command["args"][0] == "~":
-                    os.chdir(HOME)
-                # os.chdir("test_folder")
-                # os.path.isdir(directory_path)
-                elif os.path.isdir(command["args"][0]):
-                    os.chdir(command["args"][0])
-                else:
-                    print(f"cd: {command["args"][0]}: No such file or directory")
-            case _:
-                if path := find_exec(command["base"]):
-                    subprocess.run([command["base"]] + command["args"], executable=path)
-                else:
-                    print(f"{command['base']}: command not found")
-        pass
+
+        if command in commands:
+            commands[command](*command_with_args[1:])
+            continue
+        elif path := shutil.which(command):
+            subprocess.run(command_with_args)
+            continue
+
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
